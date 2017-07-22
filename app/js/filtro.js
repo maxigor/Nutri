dfd176a2f5e4b60a0eaeafe9746131ddd94be878
Max Igor <maxigor.ferreira@gmail.com>
2017-07-22 14:24:22 -0300

    filtro para busca de usuários com expressões regulares

--- app/js/filtro.js
+++ app/js/filtro.js
@@ -1,16 +1,32 @@
 var campoFiltro = document.querySelector("#filtroTabela");
 
 campoFiltro.addEventListener("input", function(){
-  console.log(this.value);
 
   var pacientes = document.querySelectorAll(".paciente");
 
-  for(posicao = 0; posicao < pacientes.length; posicao++){
+  if (this.value.length > 0) {
+
+    for(posicao = 0; posicao < pacientes.length; posicao++){
       var paciente = pacientes[posicao];
       var tdNome = paciente.querySelector(".info-nome");
       var nome = tdNome.textContent;
-      console.log(nome);
+      var expressaoRegular = new RegExp(this.value , "i");
 
+      if (expressaoRegular.test(nome)) {
+        paciente.classList.remove("ocultar");
+      } else {
+        paciente.classList.add("ocultar");
+      }
+    } //fim do for
+
+  } else {
+    for (var i = 0; i < pacientes.length; i++) {
+      paciente = pacientes[i];
+      paciente.classList.remove("ocultar");
+    }
   }
 
 });
+
+
+//se o campo filtro for igual ao nome do paciente mostre na tabela e esconda os outros e se for igual mostrar na tabela
--- app/js/remove.js
+++ app/js/remove.js
@@ -2,9 +2,9 @@ var tabela = document.querySelector("#tabela-pacientes")
 
 tabela.addEventListener("dblclick", function(event){
 
-  event.target.parentNode.classList.add(".fadeOut");
+  event.target.parentNode.classList.add("fadeOut");
 
   setTimeout(function() {
     event.target.parentNode.remove();
-  },1500);
+  },500);
 });
